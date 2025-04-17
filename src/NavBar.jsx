3b6f6f203bc5488aeba6dbc1fdf40d586a3f2100
ScottW <webster.j.scott@gmail.com>
2025-04-17 10:47:15 -0400

    Update NavBar.jsx

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import styles from "./NavBar.module.css";
 
 const NavBar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
